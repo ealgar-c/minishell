8ac2d0a610fabeb329e4db56c4aee33c775a36d4
Elisa Rivero Perez <erivero-@c3r7s3.42malaga.com>
2023-11-08 15:02:52 +0100

    few changes

--- src/extensor.c
+++ src/extensor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   extensor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 20:03:54 by ealgar-c          #+#    #+#             */
-/*   Updated: 2023/11/08 13:53:50 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:02:14 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,15 @@ char	*get_extended(char *str, int orig, t_info *info)
 	char	*ret;
 
 	len = 0;
-	while (str[orig + len] && str[orig + len] != ' ' && str[orig + len] != '$')
+	while (str[orig + len] && str[orig + len] != ' ' && str[orig + len] != '$'
+		&& str[orig + len] != '/')
 		len++;
 	env_to_check = ft_substr(str, orig, len);
 	ret = get_env(env_to_check, info);
 	return (ft_insertstr(str, orig - 1, len + 1, ret));
 }
 
-char	*check_extensor(char *content, t_info *info, int quoted)
+char	*check_extensor(char *content, t_info *info, char q)
 {
 	int		i;
 	char	*to_extend;
@@ -66,11 +67,11 @@ char	*check_extensor(char *content, t_info *info, int quoted)
 	to_extend = ft_strdup(content);
 	while (to_extend[i])
 	{
-		if (to_extend[i] == '$' && quoted != SINGLE)
+		if (to_extend[i] == '$' && q != 39)
 			to_extend = get_extended(to_extend, i + 1, info);
-		else if (to_extend[0] == '~' && quoted == NONE)
+		else if (to_extend[0] == '~' && q != 39 && q != 34)
 			to_extend = ft_strdup(getenv("HOME"));
-		if (!(to_extend[i] == '$' && quoted != SINGLE))
+		if (!(to_extend[i] == '$' && q != 39))
 			i++;
 	}
 	return (free(content), to_extend);
