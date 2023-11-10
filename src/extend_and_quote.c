fa6c465a0cb020dec8f2292b5ae139f67e3e9be7
Elisa Rivero <erivero-@erivero.local>
2023-11-10 11:49:34 +0100

    still in process

--- src/extend_and_quote.c
+++ src/extend_and_quote.c
@@ -6,98 +6,92 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 15:11:25 by ealgar-c          #+#    #+#             */
-/*   Updated: 2023/11/10 10:18:37 by erivero-         ###   ########.fr       */
+/*   Updated: 2023/11/10 11:43:28 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char	*ft_quote_handling(char *str, char *prev, int i, t_info *info)
+char	*ft_quote_handling(char *str, int *i, t_info *info)
 {
 	char	*content;
 	int		len;
 	char	q;
 
 	len = 1;
-	q = str[i];
-	while (str[i + len] && str[i + len] != q)
+	q = str[*i];
+//	ft_printf("previous: %c, current: %c, next: %c\n", str[i - 1], q, str[i + len]);
+	if (str[*i + len] == 'q')
+		return(ft_strdup(NULL));
+	while (str[*i + len] && str[*i + len] != q)
 		len++;
-	if (!str[i + len])
+//	ft_printf("len is: %d", len);
+/* 	if (!str[i + len])
 	{
 		ft_error_handling(42, "Wrong quotes, please fix", info);
-		return (free(prev), NULL);
-	}
-	if (len - i > 0) // si hay algo entre las comillas
-		content = check_extensor(ft_substr(str, i + 1, len - 1), info, q);
-	else // dice enrique que hay que alojar el nulo
-		content = ft_strdup(NULL);
-	if (prev)
-	{
-//		ft_printf("Q_h PREV was: %s, content was: %s\n", content);
-		content = ft_strjoin(prev, content);
-		free(prev);
-	}
-//	ft_printf("after quote_h: %s\n", content);
+		return (NULL);
+	} */
+	content = check_extensor(ft_substr(str, *i + 1, len - 1), info, str[*i]);
+	*i += len;
+//	content = check_extensor(ft_substr(str, i + 1, len - 1), info, q);
+//	ft_printf("after q_h: %s\n", content);
 	return (content);
 }
 
-char	*ft_content_handling(char *str, char *prev, int i, t_info *info)
+char	*ft_content_handling(char *str, int *i, t_info *info)
 {
 	char	*content;
 	int		len;
 
 	len = 0;
-	while (str[i + len] && str[i + len] != 34 && str[i + len] != 39)
+	while (str[*i + len] && str[*i + len] != 34 && str[*i + len] != 39)
 		len++;
-	content = check_extensor(ft_substr(str, i, len), info, str[i]);
-	if (prev)
-	{
-//		ft_printf("cont_h PREV was: %s, content was: %s\n", content);
-		content = ft_strjoin(prev, content);
-		free(prev);
-	}
-//	ft_printf("cont_h after joinin: %s\n", content);
+	content = check_extensor(ft_substr(str, *i, len), info, str[*i]);
+	*i += len + 1;
 	return (content);
 }
 
-char	*update_prev(char *prev, char *content) {
-    char *temp = prev;
-    if (prev) {
-        prev = ft_strjoin(prev, content);
-        free(temp);
-    } else {
-        prev = ft_strdup(content);
-    }
-    free(content);
-    return prev;
+char	*update_prev(char *prev, char *content)
+{
+	char	*temp;
+
+	temp = prev;
+	if (prev)
+	{
+		prev = ft_strjoin(prev, content);
+		free(temp);
+	}
+	else
+		prev = ft_strdup(content);
+	ft_printf("UP_PREV: prev: %s, content: %s\n", prev, content);
+	free(content);
+	return (prev);
 }
 
-char	*ft_join_content(char *str, t_info *info) {
-    char *cmd = NULL;
-    int i = 0;
+char	*ft_join_content(char *str, t_info *info)
+{
+	char	*cmd;
+	char	*prev;
+	char	*content;
+	int		i;
 
-    while (str[i] && !info->error) 
+	i = 0;
+	cmd = 0;
+	while (str[i] && !info->error)
 	{
-        char *prev = cmd;
-        char *content = NULL;
-
-        if (str[i] == 34 || str[i] == 39) {
-            content = ft_quote_handling(str, NULL, i, info);
-            i += 2; // Saltar la comilla y avanzar
-        } else {
-
-/*             while (str[j] && str[j] != 34 && str[j] != 39)
-                j++; */
-            content = ft_content_handling(str, NULL, i, info);
-            i += ft_strlen(content);
-        }
-
-        cmd = update_prev(prev, content);
-        i++;
-    }
-
-    free(str);
-    return cmd;
+		prev = cmd;
+		content = NULL;
+		if (str[i] == 34 || str[i] == 39)
+		{
+			content = ft_quote_handling(str, &i, info);
+//			i += 2;
+		}
+		else
+			content = ft_content_handling(str, &i, info);
+//		i += ft_strlen(content);
+		cmd = update_prev(prev, content);
+	}
+	return (free(str), cmd);
 }
 
 void	ft_extend_and_quotes(char **cmd, t_info *info)
